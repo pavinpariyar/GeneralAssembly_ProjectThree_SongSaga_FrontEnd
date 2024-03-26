e485199df4834da9ee976c7eb40d1657b72deb3a
EVYN ROSE <evynrosebud@gmail.com>
2024-03-26 12:04:00 +0000

    changed the code in show song to allow modal to properly close

--- src/components/Showsong.tsx
+++ src/components/Showsong.tsx
@@ -12,10 +12,11 @@ function ShowSong({ user }: { user: null | IUser }) {
     const { songId } = useParams()
     const navigate = useNavigate()
 
-    const [isActive, setIsActive] = useState(false)
+    const [isActive, setIsActive] = useState(true)
 
     const handleToggle = () => {
-        setIsActive(!isActive)
+        setIsActive(false);
+        navigate('/songs'); // Optionally, navigate to another page when closing modal
     }
 
     React.useEffect(() => {
@@ -67,50 +68,45 @@ function ShowSong({ user }: { user: null | IUser }) {
     };
 
 
-    return <section className="section">
-        <div className="modal is-active" onClick={handleToggle}>
-            <div className="modal-background"></div>
-            <div className="modal-card">
-                <header className="modal-card-head">
-                    <p className="modal-card-title">{song?.name}</p>
-                    <button className="delete" aria-label="close"></button>
-                </header>
-                <section className="modal-card-body">
-                    <div className="container">
-                        <div className="columns">
-                            <div className="column is-one-third">
-                                <figure className="image is-1by1">
-                                    <img src={song?.albumCover} alt="album cover image" />
-                                </figure>
-                            </div>
-                            <div className="column">
-                                <h1 className="title">{song?.artist}</h1>
-                                {/* <p className="subtitle">{song?.artist}</p> */}
-                                <p className="subtitle">{song?.album}</p>
-                                <div>
-                                    <YouTube videoId='_1By7t5fd6I' opts={opts} onReady={onPlayerReady} />
+    return (
+        <section className="section">
+            {isActive &&
+                <div className="modal is-active">
+                    <div className="modal-background" onClick={handleToggle}></div>
+                    <div className="modal-card">
+                        <header className="modal-card-head">
+                            <p className="modal-card-title">{song?.name}</p>
+                            <button className="delete" aria-label="close" onClick={handleToggle}></button>
+                        </header>
+                        <section className="modal-card-body">
+                            <div className="container">
+                                <div className="columns">
+                                    <div className="column is-one-third">
+                                        <figure className="image is-1by1">
+                                            <img src={song?.albumCover} alt="album cover image" />
+                                        </figure>
+                                    </div>
+                                    <div className="column">
+                                        <h1 className="title">{song?.artist}</h1>
+                                        {/* <p className="subtitle">{song?.artist}</p> */}
+                                        <p className="subtitle">{song?.album}</p>
+                                        <div>
+                                            <YouTube videoId='_1By7t5fd6I' opts={opts} onReady={onPlayerReady} />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </section>
+                        <footer className="modal-card-foot">
+                            <div className="buttons">
+                                {song && (user._id === song.user) && <button className="button is-danger" onClick={deleteSong}>Delete Song!</button>}
+                            </div>
+                        </footer>
                     </div>
-                </section>
-                <footer className="modal-card-foot">
-                    <div className="buttons">
-                        {song && (user._id === song.user) && <button className="button is-danger" onClick={deleteSong}>Delete Song!</button>}
-                    </div>
-                </footer>
-            </div>
-        </div>
-        {/* <div className="container">
-            <div className="columns is-multiline">
-                {song && <SongCards
-                    key={song._id}
-                    {...song}
-                />}
-            </div>
-            {song && (user._id === song.user) && <button onClick={deleteSong} className="button is-danger">Delete this song!</button>}
-        </div> */}
-    </section>
+                </div>
+            }
+        </section>
+    )
 }
 
 export default ShowSong
